56ff5f7ebc188fafa3c64d8d5f144e69e2961394
Tam Phan <Tam2690@gmail.com>
2023-11-04 23:32:21 +0700

    [PP-2]: Create Greeting component

---
+++ src/app/components/Greeting/index.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import GreetingStyled from './styles';
+
+const Greeting = () => <GreetingStyled>Greeting</GreetingStyled>;
+
+export default Greeting;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Greeting from './components/Greeting';
 import Header from './components/Header';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Greeting />
     </div>
   );
 }
